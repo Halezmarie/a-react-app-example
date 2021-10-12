1f42fea8b19f1cae40eb97d1238db0231f1bd0d9
halezmarie <teacherhaleymarie@gmail.com>
2021-10-12 16:49:40 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import moment from 'moment';
-import ExampleComponent from './ExampleComponent'
-import TestComponent from './TestComponent'
+import CatComponent from './CatComponent.js'
+import GraceHopperQuoteComponent from './GraceHopperQuoteComponent.js'
+import MouseComponent  from './MouseComponent.js'
+import ExampleComponent  from './ExampleComponent.js'
+import TestComponent  from './TestComponent.js'
 
 // Add your code own within the return statement
 
@@ -11,14 +13,14 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {moment().format('MMMM Do YYYY, hh:mm:ss a')}
+          {"Now"}
         </header>
-        <p className="App-intro">
-          In React apps, we write JSX - it looks like HTML, and uses a lot of HTML syntax.
-          JSX lets us include JavaScript functions right along with the HTML, and also
-          allows us to add in components, which are separate, self-contained chunks of JSX.
-        </p>
         <ExampleComponent />
+        <TestComponent />
+        <CatComponent />
+        <GraceHopperQuoteComponent />
+        <MouseComponent />
+        
 
       </div>
     );
